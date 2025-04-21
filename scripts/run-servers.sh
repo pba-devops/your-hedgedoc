8e2074b3c9d6179b24d34ccd125812b6e7f07172
baala_p <philippe.baala-prestataire@lcl.fr>
2025-04-21 20:26:44 +0200

    [ Hedgedoc ] - Starting script has been updated to consider database and app environment variables

--- scripts/run-servers.sh
+++ scripts/run-servers.sh
@@ -1,19 +1,54 @@
 #!/bin/bash
 
-sleep 2
+#set -E
 
-rm -f /run/postgresql/.s.PGSQL.5432
-rm -f /tmp/.s.PGSQL.5432
-rm -f /tmp/.s.PGSQL.5432.lock
-rc-service /etc/init.d/postgresql start
+DATABASE_LOCATION=/c/Users/baala_p/Documents/Hedgedoc/your-hedgedoc/hedgedoc-db
+DATABASE_NOT_RUNNING="no server running"
 
-sleep 20
+sleep 0
 
-if [ ! -f "/HEDGEDOC_OK" ]; then
+if [ ! -d "${DATABASE_LOCATION}" ] ; then
 
-	( cd /hedgedoc/ && ./bin/setup )
-	touch /HEDGEDOC_OK
+	echo "DATABASE SERVER CREATION"
+	initdb -D $DATABASE_LOCATION -U postgres -W -E UTF8 -A scram-sha-256
+	echo "DATABASE SERVER FIRST START"
+	pg_ctl -D "${DATABASE_LOCATION}" -l logfile start
+	echo "HEDGEDOC_DB DATABASE CREATION"
+	psql -U postgres -c 'CREATE DATABASE hedgedoc_db;'
 
 fi
 
-(cd /hedgedoc && yarn start)
+DATABASE_STATUS=$(pg_ctl -D "${DATABASE_LOCATION}" status)
+if [[ $DATABASE_STATUS == *$DATABASE_NOT_RUNNING* ]] ; then
+
+	echo "NO DATABASE RUNNING"
+	pg_ctl -D "${DATABASE_LOCATION}" -l logfile start
+
+else
+
+	echo "DATABASE ALREADY STARTED"
+
+fi
+
+sleep  2
+
+export NODE_ENV=production
+export CMD_DB_USERNAME=postgres
+export CMD_DB_PASSWORD=postgres
+export CMD_DB_DATABASE=hedgedoc_db
+export CMD_DB_HOST=localhost
+export CMD_DB_PORT=5432
+export CMD_DB_DIALECT=postgres
+export CMD_DOMAIN=localhost
+export CMD_URL_ADDPORT=true
+export CMD_PORT=3000
+export CMD_PROTOCOL_USESSL=false
+export CMD_SESSION_SECRET=hedgedoc
+env | grep CMD_
+echo "NODE_ENV: $NODE_ENV"
+
+(cd ../hedgedoc && yarn start)
+
+sleep 0
+
+#pg_ctl -D "${DATABASE_LOCATION}" -l logfile stop
